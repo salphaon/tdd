21abdc79b93a4b0115a01f160757e24afb371965
salphaon <salphaon@gmail.com>
2018-08-16 02:58:44 +0900

    9장

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,6 +1,6 @@
 public class Dollar extends Money {
-    Dollar(int amount) {
-        this.amount = amount;
+    Dollar(int amount, String currency) {
+        super(amount, currency);
     }
 
     Money times(int multiplier) {
--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,12 +1,18 @@
 abstract class Money {
     protected int amount;
+    protected String currency;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     abstract Money times(int multiplier);
@@ -15,5 +21,9 @@ abstract class Money {
         Money money = (Money) object;
         return amount == money.amount
                 && getClass().equals(money.getClass());
+    }
+
+    public String currency() {
+        return currency;
     }
 }
